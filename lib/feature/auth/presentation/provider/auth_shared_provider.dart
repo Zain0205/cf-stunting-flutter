39321e67940fb68893744cfa9903b77de242e2ff
Zain0205 <fahmizain000@gmail.com>
2026-01-05 23:49:06 +0700

    feat: add auth local datasource provider

--- lib/feature/auth/presentation/provider/auth_shared_provider.dart
+++ lib/feature/auth/presentation/provider/auth_shared_provider.dart
@@ -1,12 +1,14 @@
 // Shared providers untuk auth (dipakai oleh login, register, logout, dll)
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:mobile_flutter/core/data/local/shared_prefs_storage_service_provider.dart';
 import 'package:mobile_flutter/core/network/dio_client_provider.dart';
+import 'package:mobile_flutter/feature/auth/data/datasource/auth_local_datasource.dart';
 import 'package:mobile_flutter/feature/auth/data/datasource/auth_remote_datasource.dart';
 import 'package:mobile_flutter/feature/auth/data/repository/auth_repository_impl.dart';
 
-// final authLocalDataSourceProvider = Provider<AuthLocalDataSource>((ref) {
-//   return AuthLocalDataSource(ref.watch(storageServiceSyncProvider));
-// });
+final authLocalDataSourceProvider = Provider<AuthLocalDataSource>((ref) {
+  return AuthLocalDataSource(ref.watch(storageServiceSyncProvider));
+});
 
 final authRemoteDataSourceProvider = Provider<AuthRemoteDataSource>((ref) {
   return AuthRemoteDataSourceImpl(ref.watch(dioClientProvider));
@@ -15,6 +17,6 @@ final authRemoteDataSourceProvider = Provider<AuthRemoteDataSource>((ref) {
 final authRepositoryProvider = Provider<AuthRepositoryImpl>((ref) {
   return AuthRepositoryImpl(
     remoteDataSource: ref.watch(authRemoteDataSourceProvider),
-    // localDataSource: ref.watch(authLocalDataSourceProvider),
+    localDataSource: ref.watch(authLocalDataSourceProvider),
   );
 });
