25db4f21140eef7b1f8856d4e2ed5b64c3a521b5
Zain0205 <fahmizain000@gmail.com>
2026-01-05 23:49:28 +0700

    feat: create login usecase

---
+++ lib/feature/auth/domain/usecase/login_usecase.dart
@@ -0,0 +1,33 @@
+import 'package:dartz/dartz.dart';
+import 'package:logger/logger.dart';
+import 'package:mobile_flutter/core/error/failure.dart';
+import 'package:mobile_flutter/feature/auth/domain/entity/login_response_entity.dart';
+import 'package:mobile_flutter/feature/auth/domain/repository/auth_repository.dart';
+
+final logger = Logger();
+
+class Login {
+  final AuthRepository repository;
+
+  Login(this.repository);
+
+  Future<Either<Failure, LoginResponseEntity>> call(
+    String name,
+    String password,
+  ) async {
+    final result = await repository.login(name, password);
+
+    return result.fold((failure) => Left(failure), (loginResponse) async {
+      // Save token
+      final saveTokenResult = await repository.saveToken(loginResponse.token);
+      saveTokenResult.fold(
+        (failure) => logger.i("Error saving token: ${failure.message}"),
+        (_) => logger.i("Token saved successfully"),
+      );
+
+      // Save students
+
+      return Right(loginResponse);
+    });
+  }
+}
