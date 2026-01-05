f827d8be580830a9cc9230f9dced11e995500ca2
Zain0205 <fahmizain000@gmail.com>
2026-01-05 23:37:58 +0700

    feat: create app routes config

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,18 @@
+import 'package:go_router/go_router.dart';
+import 'package:mobile_flutter/feature/auth/presentation/screen/register_screen.dart';
+import 'package:mobile_flutter/routes/route_path.dart';
+import 'package:mobile_flutter/routes/route_transition.dart';
+
+class AppRoutes {
+  static final GoRouter router = GoRouter(
+    initialLocation: RoutePath.login,
+    debugLogDiagnostics: true,
+    routes: [
+      GoRoute(
+        path: RoutePath.login,
+        pageBuilder: (context, state) =>
+            RouteTransitions.cupertino(child: RegisterScreen(), state: state),
+      ),
+    ],
+  );
+}
