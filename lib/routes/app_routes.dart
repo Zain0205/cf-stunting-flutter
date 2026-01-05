99e1fba9f7fe01b7e38fe9b3d97728b11a098fde
Zain0205 <fahmizain000@gmail.com>
2026-01-06 00:45:24 +0700

    feat: add new routes

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,4 +1,5 @@
 import 'package:go_router/go_router.dart';
+import 'package:mobile_flutter/feature/auth/presentation/screen/login_screen.dart';
 import 'package:mobile_flutter/feature/auth/presentation/screen/register_screen.dart';
 import 'package:mobile_flutter/routes/route_path.dart';
 import 'package:mobile_flutter/routes/route_transition.dart';
@@ -10,6 +11,11 @@ class AppRoutes {
     routes: [
       GoRoute(
         path: RoutePath.login,
+        pageBuilder: (context, state) =>
+            RouteTransitions.cupertino(child: LoginScreen(), state: state),
+      ),
+      GoRoute(
+        path: RoutePath.register,
         pageBuilder: (context, state) =>
             RouteTransitions.cupertino(child: RegisterScreen(), state: state),
       ),
