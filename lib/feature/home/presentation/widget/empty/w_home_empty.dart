e292b3b8694f94bee6fd5214b943b5135f4a670b
Zain0205 <fahmizain000@gmail.com>
2026-01-06 23:24:15 +0700

    feat: create empty widget

---
+++ lib/feature/home/presentation/widget/empty/w_home_empty.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_flutter/core/widget/primary_button.dart';
+
+class WHomeEmpty extends StatelessWidget {
+  const WHomeEmpty({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height - 100,
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.only(top: 40),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.asset('assets/images/img_empty.png'),
+              const Text(
+                "Belum ada Data Skrining!",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 6),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 40),
+                child: Text(
+                  "Data skrining baru, akan muncul di halaman ini.",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 14, color: Colors.grey),
+                ),
+              ),
+              const SizedBox(height: 12),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 40),
+                child: PrimaryButton(textButton: "Mulai Skrining"),
+              ),
+              const SizedBox(height: 50),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
