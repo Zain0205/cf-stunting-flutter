8b031fd0ce16c469cf33e8ce1d7278bc76e0f98d
Zain0205 <fahmizain000@gmail.com>
2026-01-06 22:34:29 +0700

    feat: add new app routes

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,14 +1,27 @@
 import 'package:go_router/go_router.dart';
 import 'package:mobile_flutter/feature/auth/presentation/screen/login_screen.dart';
 import 'package:mobile_flutter/feature/auth/presentation/screen/register_screen.dart';
+import 'package:mobile_flutter/feature/home/presentation/screen/home_screen.dart';
+import 'package:mobile_flutter/feature/onboarding/presentation/screen/onboarding_screen.dart';
+import 'package:mobile_flutter/feature/splash/presentation/screen/splash_screen.dart';
 import 'package:mobile_flutter/routes/route_path.dart';
 import 'package:mobile_flutter/routes/route_transition.dart';
 
 class AppRoutes {
   static final GoRouter router = GoRouter(
-    initialLocation: RoutePath.login,
+    initialLocation: RoutePath.splash,
     debugLogDiagnostics: true,
     routes: [
+      GoRoute(
+        path: RoutePath.splash,
+        pageBuilder: (context, state) =>
+            RouteTransitions.cupertino(child: SplashScreen(), state: state),
+      ),
+      GoRoute(
+        path: RoutePath.onboarding,
+        pageBuilder: (context, state) =>
+            RouteTransitions.cupertino(child: OnboardingScreen(), state: state),
+      ),
       GoRoute(
         path: RoutePath.login,
         pageBuilder: (context, state) =>
@@ -18,6 +31,11 @@ class AppRoutes {
         path: RoutePath.register,
         pageBuilder: (context, state) =>
             RouteTransitions.cupertino(child: RegisterScreen(), state: state),
+      ),
+      GoRoute(
+        path: RoutePath.mainNavigation,
+        pageBuilder: (context, state) =>
+            RouteTransitions.cupertino(child: HomeScreen(), state: state),
       ),
     ],
   );
