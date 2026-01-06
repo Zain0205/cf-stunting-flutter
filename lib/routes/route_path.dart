e1bbc309f68deca1470e23c30d308a374df10b14
Zain0205 <fahmizain000@gmail.com>
2026-01-06 22:34:45 +0700

    feat: add new route path

--- lib/routes/route_path.dart
+++ lib/routes/route_path.dart
@@ -1,5 +1,7 @@
 class RoutePath {
-  static const String login = '/';
+  static const String splash = '/';
+  static const String login = '/login';
   static const String register = '/register';
   static const String onboarding = '/onboarding';
+  static const String mainNavigation = '/home';
 }
