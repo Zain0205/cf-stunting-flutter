80668b93bf3632be3e1532db17f181d2ae7f0077
Zain0205 <fahmizain000@gmail.com>
2026-03-18 20:55:14 +0700

    feat: adjust empty widget style

--- lib/feature/home/presentation/widget/empty/w_home_empty.dart
+++ lib/feature/home/presentation/widget/empty/w_home_empty.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:mobile_flutter/core/widget/primary_button.dart';
-import 'package:mobile_flutter/routes/route_path.dart';
 
 class WHomeEmpty extends StatelessWidget {
   const WHomeEmpty({super.key});
@@ -9,7 +6,6 @@ class WHomeEmpty extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: MediaQuery.of(context).size.height - 100,
       width: double.infinity,
       child: Padding(
         padding: const EdgeInsets.only(top: 0),
@@ -29,16 +25,6 @@ class WHomeEmpty extends StatelessWidget {
                   "Data skrining baru, akan muncul di halaman ini.",
                   textAlign: TextAlign.center,
                   style: TextStyle(fontSize: 14, color: Colors.grey),
-                ),
-              ),
-              const SizedBox(height: 12),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 40),
-                child: PrimaryButton(
-                  textButton: "Mulai Skrining",
-                  onTap: () {
-                    context.push(RoutePath.quisioner);
-                  },
                 ),
               ),
             ],
